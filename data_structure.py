e937129ae3c32889770ab5ad25ed0e0361334913
Carter Lim <carter.limster@gmail.com>
2025-11-19 01:12:04 -0800

    feat: added WaterSupplier class

---
+++ data_structure.py
@@ -0,0 +1,8 @@
+class WaterSupplier:
+    def __init__(self, supplier_name, region, potable_water_use, recycled_water_use, population_served, residential_use_per_capita):
+        self.supplier_name = supplier_name
+        self.region = region
+        self.potable_water_use = potable_water_use
+        self.recycled_water_use = recycled_water_use
+        self.population_served = population_served
+        self.residential_use_per_capita = residential_use_per_capita
